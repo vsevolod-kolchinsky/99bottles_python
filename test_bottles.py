22e1c1a684db99dffabc756434c468f05128907d
vsevolod kolchinsky <d@d.kiev.ua>
2024-04-04 23:50:14 +0300

    tests fixed

--- test_bottles.py
+++ test_bottles.py
@@ -3,82 +3,90 @@ from main import Bottles
 
 
 def test_first_verse():
-    expected = ("99 bottles of beer on the wall, " +
-                "99 bottles of beer.\n" +
-                "Take one down and pass it around, " +
-                "98 bottles of beer on the wall.\n")
+    expected = (
+        "99 bottles of beer on the wall, "
+        "99 bottles of beer.\n"
+        "Take one down and pass it around, "
+        "98 bottles of beer on the wall.\n"
+    )
     assert Bottles().verse(99) == expected
 
 
-@pytest.mark.skip
 def test_another_verse():
-    expected = ("3 bottles of beer on the wall, " +
-                "3 bottles of beer.\n" +
-                "Take one down and pass it around, " +
-                "2 bottles of beer on the wall.\n")
+    expected = (
+        "3 bottles of beer on the wall, "
+        "3 bottles of beer.\n"
+        "Take one down and pass it around, "
+        "2 bottles of beer on the wall.\n"
+    )
     assert Bottles().verse(3) == expected
 
 
-@pytest.mark.skip
 def test_verse_2():
-    expected = ("2 bottles of beer on the wall, " +
-                "2 bottles of beer.\n" +
-                "Take one down and pass it around, " +
-                "1 bottle of beer on the wall.\n")
+    expected = (
+        "2 bottles of beer on the wall, "
+        "2 bottles of beer.\n"
+        "Take one down and pass it around, "
+        "1 bottle of beer on the wall.\n"
+    )
     assert Bottles().verse(2) == expected
 
 
-@pytest.mark.skip
 def test_verse_1():
-    expected = ("1 bottle of beer on the wall, " +
-                "1 bottle of beer.\n" +
-                "Take it down and pass it around, " +
-                "no more bottles of beer on the wall.\n")
+    expected = (
+        "1 bottle of beer on the wall, "
+        "1 bottle of beer.\n"
+        "Take it down and pass it around, "
+        "no more bottles of beer on the wall.\n"
+    )
     assert Bottles().verse(1) == expected
 
 
-@pytest.mark.skip
 def test_verse_0():
-    expected = ("No more bottles of beer on the wall, " +
-                "no more bottles of beer.\n" +
-                "Go to the store and buy some more, " +
-                "99 bottles of beer on the wall.\n")
+    expected = (
+        "No more bottles of beer on the wall, "
+        "no more bottles of beer.\n"
+        "Go to the store and buy some more, "
+        "99 bottles of beer on the wall.\n"
+    )
     assert Bottles().verse(0) == expected
 
 
-@pytest.mark.skip
 def test_a_couple_verses():
-    expected = ("99 bottles of beer on the wall, " +
-                "99 bottles of beer.\n" +
-                "Take one down and pass it around, " +
-                "98 bottles of beer on the wall.\n" +
-                "\n" +
-                "98 bottles of beer on the wall, " +
-                "98 bottles of beer.\n" +
-                "Take one down and pass it around, " +
-                "97 bottles of beer on the wall.\n")
+    expected = (
+        "99 bottles of beer on the wall, "
+        "99 bottles of beer.\n"
+        "Take one down and pass it around, "
+        "98 bottles of beer on the wall.\n"
+        "\n"
+        "98 bottles of beer on the wall, "
+        "98 bottles of beer.\n"
+        "Take one down and pass it around, "
+        "97 bottles of beer on the wall.\n"
+    )
     assert Bottles().verses(99, 98) == expected
 
 
-@pytest.mark.skip
 def test_a_few_verses():
-    expected = ("2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n" +
-      "\n" +
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n" +
-      "\n" +
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n")
-    assert Bottles().verses(2, 0)
+    expected = (
+        "2 bottles of beer on the wall, "
+        "2 bottles of beer.\n"
+        "Take one down and pass it around, "
+        "1 bottle of beer on the wall.\n"
+        "\n"
+        "1 bottle of beer on the wall, "
+        "1 bottle of beer.\n"
+        "Take it down and pass it around, "
+        "no more bottles of beer on the wall.\n"
+        "\n"
+        "No more bottles of beer on the wall, "
+        "no more bottles of beer.\n"
+        "Go to the store and buy some more, "
+        "99 bottles of beer on the wall.\n"
+    )
+    assert Bottles().verses(2, 0) == expected
 
-@pytest.mark.skip
+
 def test_the_whole_song():
     expected = """\
 99 bottles of beer on the wall, 99 bottles of beer.
